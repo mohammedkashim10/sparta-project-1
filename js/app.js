c862f3945f1a172d412f41a59393c5d69e0c6398
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-09-17 13:59:29 +0100

    play button functionality done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // Getting the play button
+  const play = document.getElementById('play');
   // Getting the instructions button
   const instructions = document.getElementById('instruct');
   // Getting the instructions modal
@@ -7,8 +9,38 @@ document.addEventListener('DOMContentLoaded', () => {
   // Getting the <span> element that closes the modal (x button)
   const span = document.getElementsByClassName("close")[0];
 
-  // Adding a click event to the instructions button to display the instructions modal
-  instructions.addEventListener('click', (event) => {
+  // Creating the function for the play button
+  const playbtn = () => {
+    const title = document.getElementById('title'); // Getting the game title
+    title.style.display = 'none'; // Hides the title
+    play.style.display = 'none'; // Hides the play button
+    instructions.style.display = 'none'; // Hides the instructions button
+
+    const time = document.createElement('h4');
+    time.innerHTML = 'timer goes here';
+    document.body.appendChild(time);
+
+    const select = document.createElement('h1'); // <h1 id="select"></h1>
+    select.innerHTML = 'select -text/color-';
+    document.body.appendChild(select);
+    const color = document.createElement('h1'); // <h1 id="color"></h1>
+    color.innerHTML = 'colored word appears here';
+    document.body.appendChild(color);
+    const text = document.createElement('button'); // <button id="text"></button>
+    text.innerHTML = 'corresponds to text of colored word';
+    document.body.appendChild(text);
+    const fill = document.createElement('button'); // <button id="fill"></button>
+    fill.innerHTML = 'corresponds to color of colored word';
+    document.body.appendChild(fill);
+
+  }
+
+
+
+
+
+  // Creating the function for the instructions button: displays instructions modal
+  const instruct = () => {
     modal.style.display = 'block';
     // When the user clicks on <span> (i.e. x button), close the modal
     span.addEventListener('click', (event) => {
@@ -16,10 +48,21 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     // Closing the modal when click anywhere outside of it
     window.addEventListener('click', (event) => {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
     })
+  }
+  // Adding a click event to the play button to run the playbtn function
+  play.addEventListener('click', (event) => {
+    playbtn();
+  })
+
+
+  // Adding a click event to the instructions butto to run the instruct function
+  instructions.addEventListener('click', (event) => {
+    // Invoking the function for the instructions button
+    instruct();
   })
 
 
